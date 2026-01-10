cb1ccb9910eabbee38a65b014364b202d254f64e
Aman dhoni <parleaman@gmail.com>
2026-01-10 23:40:37 +0530

    arithmetic operator

---
+++ Lecture_2/arithematic_operator.js
@@ -0,0 +1,15 @@
+// arithmetic operator
+
+let a=4
+let b=5
+
+console.log("a = "+a+ " & "+"b = "+b);
+
+console.log("a + b = ",a+b); //addition
+console.log("a - b =", a-b);
+console.log("a * b = ",a*b);
+console.log("a / b = ",a/b);
+
+
+
+
