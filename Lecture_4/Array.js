c1c2cc962ca9db255a3b0bfbce45fddd04026bf8
Aman dhoni <parleaman@gmail.com>
2026-01-13 20:29:30 +0530

    printng of array

---
+++ Lecture_4/Array.js
@@ -0,0 +1,13 @@
+let marks=[78,89,65,69,90];
+
+// for loop
+// for(let i=0; i<marks.length; i++){
+//     console.log(marks[i]);
+    
+// }
+
+// for of loop
+for(let ele of marks){
+    console.log(ele);
+    
+}
