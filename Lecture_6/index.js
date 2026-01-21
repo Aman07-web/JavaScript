5c299adc6bfaf57b25a183b7f85cc52f7d8d2784
Aman dhoni <parleaman@gmail.com>
2026-01-21 22:20:54 +0530

    commit q3

--- Lecture_6/index.js
+++ Lecture_6/index.js
@@ -1,5 +1,15 @@
 // let head2=document.getElementById("heading");
 // console.log(head2);
 
-const headings=document.getElementsByClassName("heading-class");
-console.log(headings);
+// const headings=document.getElementsByClassName("heading-class");
+// console.log(headings);
+
+// let h2=document.querySelector("h2");
+// h2.innerText=h2.innerText+"from apnacollege students";
+// console.log(h2.innerText);
+
+let boxes=document.querySelectorAll(".box");
+
+boxes[0].innerText="Box 1";
+boxes[1].innerText="Box 2";
+boxes[2].innerText="Box 3";
