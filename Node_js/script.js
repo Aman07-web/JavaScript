5389fe81f0dc5af3fc3741be4e244c9c22fbcc31
Aman dhoni <parleaman@gmail.com>
2026-01-24 19:59:41 +0530

    learning node js

---
+++ Node_js/script.js
@@ -0,0 +1,10 @@
+console.log("aman");
+function sum(){
+    console.log(1+3);
+    
+}
+// setTimeout(sum, 5000)
+setInterval(sum,1000)
+
+console.log("gla");
+
---
+++ Node_js/server.js
@@ -0,0 +1,2 @@
+const server=require('http');
+server.createServer().listen(4800);
