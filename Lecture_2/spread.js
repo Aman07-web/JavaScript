b03675ff4a254cbdef635cd0a8190e4b0a69d2d3
Aman dhoni <parleaman@gmail.com>
2026-02-08 06:42:19 +0530

    switch statement

---
+++ Lecture_2/spread.js
@@ -0,0 +1,23 @@
+// const arr=[1,2,3,4,5,6,7];
+// console.log(...arr);
+
+// let user1 = { name: "Aman", age: 20 };
+
+// let user2 = { ...user1 };
+// console.log(user2);
+
+// merge
+// let a = [1, 2];
+// let b = [3, 4];
+
+// let c = [...a, ...b];
+// console.log(c); // [1, 2, 3, 4]
+
+// function add(...numbers) {
+//   console.log(numbers);
+// }
+// add(1, 2, 3); // [1, 2, 3]
+let nums = [10, 20, 30];
+
+console.log(add(...nums)); // 60
+
---
+++ Lecture_2/switcch.js
@@ -0,0 +1,16 @@
+let marks=75;
+
+switch(true){
+    case marks <=90 && marks >=80:
+        console.log("excelent marks");
+        break;
+    case marks <80 && marks >=70:
+        console.log("very good");
+        break;
+    case marks <70 && marks >=60:
+        console.log("good");
+        break;
+    default:
+        console.log("study hard");
+        break;
+}
