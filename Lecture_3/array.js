a99115c144df4c5a0b8d4c0e41f8a8efbad527d8
Aman dhoni <parleaman@gmail.com>
2026-02-09 20:32:55 +0530

    learning array

---
+++ Lecture_3/array.js
@@ -0,0 +1,17 @@
+// let ar=[2,45,56,78,43];
+
+// console.log(ar);
+// for(let i=0; i<ar.length; i++){
+//     console.log(ar[i]);
+// }
+
+let arr=new Array();  // another way of creating array
+arr[0]="Aman";
+arr[1]=23;
+arr[0]="virat" // changing values
+console.log(arr);
+delete arr[1]
+console.log(arr);
+
+
+
